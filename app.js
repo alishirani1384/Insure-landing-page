84e55e6013ce1d9112e1d18ab53bf3331e4b9831
Ali Shirani <alishiranii1384@gmail.com>
2023-02-02 11:17:10 +0330

    main section completed

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 const menuBtn = document.querySelector(".menu-btn");
 
 menuBtn.addEventListener("click", () => {
-    const menu = document.querySelector(".nav-links");
-    const icon = menuBtn.querySelector("img");
-    menu.classList.toggle("active");
-    if (menu.classList.contains("active")) {
-        icon.src = './images/icon-close.svg';
-        document.body.style.overflow = "hidden";
-    } else {
-        icon.src = "./images/icon-hamburger.svg";
-        document.body.style.overflow = "auto";
-    }
-})
+  const menu = document.querySelector(".nav-links");
+  const icon = menuBtn.querySelector("img");
+  menu.classList.toggle("active");
+  if (menu.classList.contains("active")) {
+    icon.src = "./images/icon-close.svg";
+    document.body.style.overflow = "hidden";
+  } else {
+    icon.src = "./images/icon-hamburger.svg";
+    document.body.style.overflow = "auto";
+  }
+});
